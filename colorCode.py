c27c4a4dda5b890faa50da5b9ce235cf4ed28d7a
relixilon <relixilon@gmail.com>
2022-10-06 12:15:43 +0100

    change case to if

--- colorCode.py
+++ colorCode.py
@@ -9,30 +9,29 @@ def colorCode(boardId):
         cards = getCards(i['id'])
         for card in cards:
             place = card['name'].split()[0]
-            match place:
-                case "CASA":
-                    updateCardCoverColor(card['id'], 'sky')
-                case "CASTILLO":
-                    updateCardCoverColor(card['id'], 'red')
-                case "ISABEL":
-                    updateCardCoverColor(card['id'], 'blue')
-                case "SANTA":
-                    updateCardCoverColor(card['id'], 'pink')
-                case "SAN":
-                    updateCardCoverColor(card['id'], 'yellow')
-                case "SARDOY":
-                    updateCardCoverColor(card['id'], 'lime')
-                case "OFICINA":
-                    updateCardCoverColor(card['id'], 'red')
-                case "MARQUES":
-                    updateCardCoverColor(card['id'], 'purple')
-                case "BRO":
-                    updateCardCoverColor(card['id'], 'black')
-                    updateCardCoverBrightness(card['id'], 'dark')
-                case "CONTINENTAL":
-                    updateCardCoverColor(card['id'], 'orange')
-                case "JAUJA":
-                    updateCardCoverColor(card['id'], 'green')
+            if place == "CASA":
+                updateCardCoverColor(card['id'], 'sky')
+            if place == "CASTILLO":
+                updateCardCoverColor(card['id'], 'red')
+            if place == "ISABEL":
+                updateCardCoverColor(card['id'], 'blue')
+            if place == "SANTA":
+                updateCardCoverColor(card['id'], 'pink')
+            if place == "SAN":
+                updateCardCoverColor(card['id'], 'yellow')
+            if place == "SARDOY":
+                updateCardCoverColor(card['id'], 'lime')
+            if place == "OFICINA":
+                updateCardCoverColor(card['id'], 'red')
+            if place == "MARQUES":
+                updateCardCoverColor(card['id'], 'purple')
+            if place == "BRO":
+                updateCardCoverColor(card['id'], 'black')
+                updateCardCoverBrightness(card['id'], 'dark')
+            if place == "CONTINENTAL":
+                updateCardCoverColor(card['id'], 'orange')
+            if place == "JAUJA":
+                updateCardCoverColor(card['id'], 'green')
 
-                # x = updateCardCoverColor(card['id'], 'red')
+
 colorCode(BOARD)
